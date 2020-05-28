500b07e7b9b9dd491008f810dbac7139147dd52f
Despina Karavia <33006682+DesKar@users.noreply.github.com>
2020-05-29 00:14:49 +0300

    Add Hotel component

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,41 @@
 <script>
-	export let name;
+  import Hotel from "./components/Hotel.svelte";
+
+  let hotels = [];
+  let roomTypes = [];
+
+  fetch("data.json")
+    .then(response => response.json())
+    .then(data => {
+      hotels = data[1].entries;
+      roomTypes = data[0].roomtypes;
+    });
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    text-align: center;
+    padding: 1em;
+    max-width: 240px;
+    margin: 0 auto;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
+  h1 {
+    color: #ff3e00;
+    text-transform: uppercase;
+    font-size: 4em;
+    font-weight: 100;
+  }
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  @media (min-width: 640px) {
+    main {
+      max-width: none;
+    }
+  }
 </style>
+
+<main>
+  {#each hotels as hotel}
+    <Hotel {...hotel} />
+  {/each}
+</main>
---
+++ src/components/Hotel.svelte
@@ -0,0 +1,13 @@
+<script>
+  export let hotelName;
+  export let rating;
+  export let city;
+  export let thumbnail;
+  export let guestrating;
+  export let ratings;
+  export let mapurl;
+  export let filters;
+  export let price;
+</script>
+
+<p>name: {hotelName}</p>
